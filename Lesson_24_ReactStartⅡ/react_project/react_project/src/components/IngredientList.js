4206ff0791c4761b0c4619e7accab2c2e9c8378c
JessieYANG <jessieyang0313@hotmail.com>
2022-10-17 23:46:17 +1030

    first commit

---
+++ Lesson_24_ReactStartⅡ/react_project/react_project/src/components/IngredientList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Ingredient from './Ingredient'
+export default function IngredientList({ ingredients }) {
+
+  const ingredientElement = ingredients.map(ingredient =>
+    <Ingredient key={ingredient.id} {...ingredient} />)
+
+  return (
+    <div className='ingredient-grid'>
+      {ingredientElement}
+    </div>
+  )
+}
